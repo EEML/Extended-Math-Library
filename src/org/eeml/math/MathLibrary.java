c50c8b61790db8903adb1a6ecd738ec3d05d853e
EULIR <runmingli_edu@163.com>
2018-04-04 19:28:22 +0800

    update file name

---
+++ src/org/eeml/math/MathLibrary.java
@@ -0,0 +1,16 @@
+package org.eeml.math;
+
+public class MathLibrary
+{
+	/**
+	 * Return greatest common divisor of the argument
+	 *
+	 * @param a first parameter
+	 * @param b second parameter
+	 * @return greatest common divisor of the argument
+	 */
+	public static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a % b);
+	}
+}
---
+++ test/org/eeml/math/MathLibraryTest.java
@@ -0,0 +1,9 @@
+package org.eeml.math;
+
+public class MathLibraryTest
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
