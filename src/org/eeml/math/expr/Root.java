a4507f2aff673c0812a8df5bcfbce55f089726d5
EULIR <runmingli_edu@163.com>
2018-04-07 21:22:22 +0800

    update root

---
+++ src/org/eeml/math/expr/Root.java
@@ -0,0 +1,12 @@
+package org.eeml.math.expr;
+
+public class Root
+{
+	private int coefficient;
+	private int base;
+
+	public Root(int base)
+	{
+
+	}
+}
---
+++ test/org/eeml/math/expr/RootTest.java
@@ -0,0 +1,10 @@
+package org.eeml.math.expr;
+
+import org.junit.Test;
+
+public class RootTest
+{
+	@Test
+	public void testToString()
+	{}
+}
