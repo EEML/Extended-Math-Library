9cfea41aa0f7529ab2330959a031b41dee8f1a55
EULIR <runmingli_edu@163.com>
2018-04-30 20:10:21 +0800

    update test

--- test/org/eeml/math/equation/LinearEquationOneTest.java
+++ test/org/eeml/math/equation/LinearEquationOneTest.java
@@ -30,6 +30,8 @@ public class LinearEquationOneTest
 	LinearEquationOne line5 = new LinearEquationOne(-1, 4, 9);
 	LinearEquationOne line6 = new LinearEquationOne(-1, 0);
 	LinearEquationOne line7 = new LinearEquationOne(2.432, 31.234);
+	LinearEquationOne line8 = new LinearEquationOne(1, 8);
+	LinearEquationOne line9 = new LinearEquationOne(2, 0, 8);
 
 	@Test
 	public void testToString()
@@ -41,6 +43,8 @@ public class LinearEquationOneTest
 		assertEquals("-x+4=9\tx=-5", line5.toString());
 		assertEquals("-x=0\tx=0", line6.toString());
 		assertEquals("2.432x+31.234=0\tx=-12.843", line7.toString());
+		assertEquals("x+8=0\tx=-8", line8.toString());
+		assertEquals("2x=8\tx=4", line9.toString());
 	}
 
 	@Test
