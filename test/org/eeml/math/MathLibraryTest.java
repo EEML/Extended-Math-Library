e4eb43c056caaab2f61f308e6625e5cc2ce17b5e
EULIR <runmingli_edu@163.com>
2018-05-02 23:15:42 +0800

    update test

--- test/org/eeml/math/MathLibraryTest.java
+++ test/org/eeml/math/MathLibraryTest.java
@@ -1,9 +1,16 @@
 package org.eeml.math;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MathLibraryTest
 {
-	public static void main(String[] args)
+	@Test
+	public void testGCD()
 	{
-
+		assertEquals(1, MathLibrary.gcd(4, 5));
+		assertEquals(1, MathLibrary.gcd(1, 5));
+		assertEquals(2, MathLibrary.gcd(4, 6));
 	}
 }
