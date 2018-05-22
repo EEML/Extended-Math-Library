a6a5fcd6047f90eb7c5c606fe9166da77b93b627
EULIR <runmingli_edu@163.com>
2018-05-22 23:11:51 +0800

    update complex number

--- src/org/eeml/math/expr/ComplexNumber.java
+++ src/org/eeml/math/expr/ComplexNumber.java
@@ -1,5 +1,17 @@
 package org.eeml.math.expr;
 
+import java.text.MessageFormat;
+
+/**
+ * This class mainly implements the math expression complex number including methods like add,
+ * multiply etc.The naming principle is the same as java.math.BigInteger.
+ * Maintained by<a href="https://github.com/EEML">EEML</a>.see more on<code>eeml.github.io</code>
+ *
+ * @author EULIR
+ * @see org.eeml.math;
+ * @since v1.0.0
+ */
+
 public class ComplexNumber
 {
 	private double real;
@@ -24,5 +36,18 @@ public class ComplexNumber
 	public ComplexNumber add(double a)
 	{
 		return new ComplexNumber(this.real + a, this.imaginary);
+	}
+
+	/**
+	 * toString method overridden from super class
+	 *
+	 * @return return String formed in
+	 * real + imaginary + "i"
+	 */
+	public String toString()
+	{
+		if (this.imaginary == 0)
+			return MessageFormat.format("{0}", this.real);
+		return MessageFormat.format("{0}" + (this.imaginary > 0 ? "+" : "") + (this.imaginary == 1 ? "" : "{1}") + "i", this.real, this.imaginary);
 	}
 }
