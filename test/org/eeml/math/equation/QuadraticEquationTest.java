5e50e09d807d9a8de6d343036102e0e9c30e01c2
EULIR <runmingli_edu@163.com>
2018-06-05 22:30:07 +0800

    add test

---
+++ test/org/eeml/math/equation/QuadraticEquationTest.java
@@ -0,0 +1,13 @@
+package org.eeml.math.equation;
+
+import org.junit.Test;
+
+public class QuadraticEquationTest
+{
+	QuadraticEquation quadraticEquation1 = new QuadraticEquation(1, 2, 1);
+
+	@Test
+	public void testToString()
+	{
+	}
+}
