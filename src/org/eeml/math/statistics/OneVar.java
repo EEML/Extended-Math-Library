a45a310e75712bf913f3e9e6b7ef73bcf476126b
EULIR <runmingli_edu@163.com>
2018-06-13 22:51:30 +0800

    update methods

--- src/org/eeml/math/statistics/OneVar.java
+++ src/org/eeml/math/statistics/OneVar.java
@@ -1,10 +1,7 @@
 package org.eeml.math.statistics;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class OneVar
 {
@@ -97,36 +94,61 @@ public class OneVar
 	public double getMedian()
 	{
 		int len = temp.length;
-		if (len % 2 == 0)
+		if ((len & 1) == 0)
 			return (temp[len >> 2] + temp[len >> 2 + 1]) / 2;
 		return temp[len >> 2];
 	}
 
 	public double getQ1()
 	{
-		return 0;
+		Double[] arr = Arrays.copyOfRange(temp, 0, temp.length / 2);
+		int len = arr.length;
+		if ((len & 1) == 0)
+			return (arr[len >> 2] + arr[len >> 2 + 1]) / 2;
+		return arr[len >> 2];
 	}
 
 	public double getQ3()
 	{
-		return 0;
+		Double[] arr = (temp.length & 1) == 0 ? Arrays.copyOfRange(temp, temp.length / 2, temp.length) : Arrays.copyOfRange(temp, temp.length / 2 + 1, temp.length);
+		int len = arr.length;
+		if ((len & 1) == 0)
+			return (arr[len >> 2] + arr[len >> 2 + 1]) / 2;
+		return arr[len >> 2];
 	}
 
 	@Override
 	public String toString()
 	{
-		return MessageFormat.format("mean={0}\n", getMean()) +
-				MessageFormat.format("sum={0}\n", getSum()) +
-				MessageFormat.format("sum^2={0}\n", getSumSquared()) +
-				MessageFormat.format("sample SD={0}\n", getSampleStandardDeviation()) +
-				MessageFormat.format("SD={0}\n", getStandardDeviation()) +
-				MessageFormat.format("n={0}\n", getTotality()) +
-				MessageFormat.format("min={0}\n", getMin()) +
-				MessageFormat.format("max={0}\n", getMax()) +
-				MessageFormat.format("mode={0}\n", getMode()) +
-				MessageFormat.format("median={0}\n", getMedian()) +
-				MessageFormat.format("Q1={0}\n", getQ1()) +
-				MessageFormat.format("Q3={0}\n", getQ3());
+		return MessageFormat.format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}",
+				MessageFormat.format("mean={0}\n", getMean()),
+				MessageFormat.format("sum={0}\n", getSum()),
+				MessageFormat.format("sum^2={0}\n", getSumSquared()),
+				MessageFormat.format("sample SD={0}\n", getSampleStandardDeviation()),
+				MessageFormat.format("SD={0}\n", getStandardDeviation()),
+				MessageFormat.format("n={0}\n", getTotality()),
+				MessageFormat.format("min={0}\n", getMin()),
+				MessageFormat.format("max={0}\n", getMax()),
+				MessageFormat.format("mode={0}\n", getMode()),
+				MessageFormat.format("median={0}\n", getMedian()),
+				MessageFormat.format("Q1={0}\n", getQ1()),
+				MessageFormat.format("Q3={0}\n", getQ3()));
 	}
 
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this)
+			return true;
+		if (!(obj instanceof OneVar))
+			return false;
+		OneVar oneVar = (OneVar) obj;
+		return Arrays.equals(oneVar.temp, this.temp);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hashCode(temp);
+	}
 }
