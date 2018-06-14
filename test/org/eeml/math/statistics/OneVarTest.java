c47fc15e4422ffd95c9659ab58822c86e61beefc
EULIR <runmingli_edu@163.com>
2018-06-14 23:02:16 +0800

    add test

---
+++ test/org/eeml/math/statistics/OneVarTest.java
@@ -0,0 +1,15 @@
+package org.eeml.math.statistics;
+
+import org.junit.Test;
+
+public class OneVarTest
+{
+	OneVar oneVar0 = new OneVar();
+	OneVar oneVar1 = new OneVar();
+
+	@Test
+	public void testGetMean()
+	{
+
+	}
+}
