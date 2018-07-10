5ca728181a612ed6dedfa477b460f7633d4449e1
EULIR <runmingli_edu@163.com>
2018-07-10 18:43:54 +0800

    update chi-squared test

--- src/org/eeml/math/statistics/test/ChiSquaredTest.java
+++ src/org/eeml/math/statistics/test/ChiSquaredTest.java
@@ -80,16 +80,112 @@ public class ChiSquaredTest {
 		return expected;
 	}
 
+
+	/**
+	 * @return p-value
+	 */
+	public double getPValue() {
+		if (chiSquared < 0 || df < 1) {
+			return 0.0;
+		}
+		double k = ((double) df) * 0.5;
+		double v = chiSquared * 0.5;
+		if (df == 2) {
+			return Math.exp(-1.0 * v);
+		}
+		double log = log(k, v);
+		if (Math.exp(log) <= 1e-8
+				|| Double.isNaN(Math.exp(log))
+				|| Double.isInfinite(Math.exp(log))) {
+			return 1e-14;
+		}
+		double gamma = Math.log(getApproxGamma(k));
+		log -= gamma;
+		if (Math.exp(log) > 1) {
+			return 1e-14;
+		}
+		return 1.0 - Math.exp(log);
+	}
+
+	/**
+	 * Incomplete gamma function
+	 * In mathematics, the upper incomplete gamma function and lower incomplete gamma
+	 * function are types of special functions, which arise as solutions to various mathematical
+	 * problems such as certain integrals.
+	 * This is a private method only for calculating p-value use.
+	 *
+	 * @param n parameter
+	 * @return gamma function value
+	 */
+	private static double getApproxGamma(double n) {
+		final double RECIP_E = 0.36787944117144232159552377016147D;
+		final double TWOPI = 6.283185307179586476925286766559D;
+		double d = 1.0 / (10.0 * n);
+		d = 1.0 / ((12 * n) - d);
+		d = (d + n) * RECIP_E;
+		d = Math.pow(d, n);
+		d *= Math.sqrt(TWOPI / n);
+		return d;
+	}
+
+	/**
+	 * log function
+	 * This is a private method only for calculating p-value use.
+	 *
+	 * @param s parameter
+	 * @param z parameter
+	 * @return log function value
+	 */
+	private static double log(double s, double z) {
+		if (z < 0.0) {
+			return 0.0;
+		}
+		double sc = (Math.log(z) * s) - z - Math.log(s);
+		double k = KM(s, z);
+		return Math.log(k) + sc;
+	}
+
+	/**
+	 * Confluent hyper-geometric function
+	 * In mathematics, a confluent hyper-geometric function is a solution of a confluent
+	 * hyper-geometric equation, which is a degenerate form of a hyper-geometric differential
+	 * equation where two of the three regular singularities merge into an irregular singularity.
+	 * This is a private method only for calculating p-value use.
+	 *
+	 * @param s parameter
+	 * @param z parameter
+	 * @return confluent hyper-geometric function value
+	 */
+	private static double KM(double s, double z) {
+		double sum = 1.0;
+		double nom = 1.0;
+		double d = 1.0;
+		double log_nom = Math.log(nom);
+		double log_d = Math.log(d);
+		double log_s;
+		double log_z = Math.log(z);
+		for (int i = 0; i < 1000; ++i) {
+			log_nom += log_z;
+			s++;
+			log_s = Math.log(s);
+			log_d += log_s;
+			double log_sum = log_nom - log_d;
+			sum += Math.exp(log_sum);
+		}
+		return sum;
+	}
+
 	/**
 	 * toString method overridden from super class
 	 *
 	 * @return return String formed in
 	 * "df=
-	 * χ2="
+	 * χ2=
+	 * p="
 	 */
 	@Override
 	public String toString() {
-		return MessageFormat.format("df={0}\nχ2={1}", getDf(), getChiSquared());
+		return MessageFormat.format("df={0}\nχ2={1}\np={2}", getDf(), getChiSquared(), getPValue());
 	}
 
 	/**
@@ -109,7 +205,7 @@ public class ChiSquaredTest {
 	 * @return true if two observed matrix are equal, otherwise false.
 	 */
 	@Override
-	public boolean equals(Object obj){
+	public boolean equals(Object obj) {
 		if (obj == this)
 			return true;
 		if (!(obj instanceof ChiSquaredTest))
