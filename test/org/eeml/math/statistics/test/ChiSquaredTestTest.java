e60505c6b697bd0d0cad2b0532130fb4551d1102
EULIR <runmingli_edu@163.com>
2018-07-10 23:25:09 +0800

    add tests

---
+++ test/org/eeml/math/statistics/test/ChiSquaredTestTest.java
@@ -0,0 +1,34 @@
+package org.eeml.math.statistics.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChiSquaredTestTest {
+	ChiSquaredTest test1 = new ChiSquaredTest(new double[][]{
+			{15, 95},
+			{85, 5}
+	});
+	ChiSquaredTest test2 = new ChiSquaredTest(new double[][]{
+			{55, 45},
+			{43, 66}
+	});
+
+	@Test
+	public void testGetDF() {
+		assertEquals(1, test1.getDf());
+		assertEquals(1, test2.getDf());
+	}
+
+	@Test
+	public void testGetChiSquared() {
+		assertEquals(129.2929, test1.getChiSquared(), 0.001);
+		assertEquals(5.06419, test2.getChiSquared(), 0.001);
+	}
+
+	@Test
+	public void testGetPValue() {
+		assertEquals(5.851314e-30, test1.getPValue(), 0.001);
+		assertEquals(0.024425, test2.getPValue(), 0.001);
+	}
+}
