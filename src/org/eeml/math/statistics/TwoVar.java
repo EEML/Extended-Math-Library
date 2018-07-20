f0454d4bc9db4ea75cf0a4dcc9a69767b6929e03
EULIR <runmingli_edu@163.com>
2018-07-20 23:51:46 +0800

    write methods

--- src/org/eeml/math/statistics/TwoVar.java
+++ src/org/eeml/math/statistics/TwoVar.java
@@ -1,7 +1,85 @@
 package org.eeml.math.statistics;
 
+import org.eeml.math.exception.InvalidDimException;
+
+import java.util.ArrayList;
+
 public class TwoVar {
-	public TwoVar(){
-		
+	private ArrayList<Double> list1;
+	private ArrayList<Double> list2;
+
+	private OneVarDouble statList1;
+	private OneVarDouble statList2;
+
+	public TwoVar(ArrayList<Double> x, ArrayList<Double> y) {
+		this.list1 = x;
+		this.list2 = y;
+		statList1 = new OneVarDouble(x);
+		statList2 = new OneVarDouble(y);
+		if (statList1.getTotality() != statList2.getTotality()) {
+			statList1 = null;
+			statList2 = null;
+			throw new InvalidDimException("Dim mismatch");
+		}
+	}
+
+	public double getXMean() {
+		return statList1.getMean();
+	}
+
+	public double getXSum() {
+		return statList1.getSum();
+	}
+
+	public double getXSumSquared() {
+		return statList1.getSumSquared();
+	}
+
+	public double getXSampleStandardDeviation() {
+		return statList1.getSampleStandardDeviation();
+	}
+
+	public double getXStandardDeviation() {
+		return statList1.getStandardDeviation();
+	}
+
+	public double getMinX() {
+		return statList1.getMin();
+	}
+
+	public double getMaxX() {
+		return statList1.getMax();
+	}
+
+	public double getYMean() {
+		return statList2.getMean();
+	}
+
+	public double getYSum() {
+		return statList2.getSum();
+	}
+
+	public double getYSumSquared() {
+		return statList2.getSumSquared();
+	}
+
+	public double getYSampleStandardDeviation() {
+		return statList2.getSampleStandardDeviation();
+	}
+
+	public double getYStandardDeviation() {
+		return statList2.getStandardDeviation();
+	}
+
+	public double getMinY() {
+		return statList2.getMin();
+	}
+
+	public double getMaxY() {
+		return statList2.getMax();
+	}
+
+	public int getTotility() {
+		return statList1.getTotality();
 	}
 }
