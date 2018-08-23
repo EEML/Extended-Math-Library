e2f5778d08cb8076390d68e28e3106988f32694d
EULIR <runmingli_edu@163.com>
2018-08-23 23:06:02 +0800

    vector

--- src/org/eeml/math/expr/Vector.java
+++ src/org/eeml/math/expr/Vector.java
@@ -13,6 +13,64 @@ public class Vector {
 		list.addAll(Arrays.asList(arguments));
 	}
 
+	private Vector(ArrayList<Double> list) {
+		this.list = list;
+	}
+
+	public Vector add(double c) {
+		ArrayList<Double> alter = new ArrayList<>();
+		for (Double aList : list) {
+			alter.add(aList + c);
+		}
+		return new Vector(alter);
+	}
+
+	public Vector add(Vector v) {
+		if (v.getDimension() != this.getDimension())
+			throw new IllegalArgumentException("dimension mismatch");
+		ArrayList<Double> alter = new ArrayList<>();
+		for (int i = 0; i < v.getDimension(); i++) {
+			alter.add(this.list.get(i) + v.list.get(i));
+		}
+		return new Vector(alter);
+	}
+
+	public Vector subtract(double c) {
+		ArrayList<Double> alter = new ArrayList<>();
+		for (Double aList : list) {
+			alter.add(aList - c);
+		}
+		return new Vector(alter);
+	}
+
+	public Vector subtract(Vector v) {
+		if (v.getDimension() != this.getDimension())
+			throw new IllegalArgumentException("dimension mismatch");
+		ArrayList<Double> alter = new ArrayList<>();
+		for (int i = 0; i < v.getDimension(); i++) {
+			alter.add(this.list.get(i) - v.list.get(i));
+		}
+		return new Vector(alter);
+	}
+
+	public Vector multiply(double c) {
+		ArrayList<Double> alter = new ArrayList<>();
+		for (Double aList : list) {
+			alter.add(aList * c);
+		}
+		return new Vector(alter);
+	}
+
+	public double multiply(Vector v) {
+		if (v.getDimension() != this.getDimension())
+			throw new IllegalArgumentException("dimension mismatch");
+		double mul = 0;
+		for (int i = 0; i < v.getDimension(); i++) {
+			mul += v.list.get(i) * this.list.get(i);
+		}
+		return mul;
+	}
+
 	public int getDimension() {
 		return list.size();
 	}
