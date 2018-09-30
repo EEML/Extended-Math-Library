607e9cdc44b78cbdf8a34506821195e924f7b42d
EULIR <runmingli_edu@163.com>
2018-09-30 23:29:02 +0800

    graph

--- src/org/eeml/math/expr/shape/Line.java
+++ src/org/eeml/math/expr/shape/Line.java
@@ -1,5 +1,6 @@
 package org.eeml.math.expr.shape;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.text.MessageFormat;
@@ -15,9 +16,22 @@ public class Line {
 		this.c = c;
 	}
 
-	public void print() {
+	public void print(int width, int length) {
 		Graphics2D graphic = new BufferedImage(400, 400, BufferedImage.TYPE_4BYTE_ABGR).createGraphics();
 		graphic.drawLine(0, (int) (-c / b), 400, (int) (-c / b - (400 * a) / b));
+		new JFrame("Preview") {{
+			setLayout(new BorderLayout());
+			add(new JComponent() {
+				@Override
+				protected void paintComponent(Graphics g) {
+					//g.drawImage();
+					super.paintComponent(g);
+				}
+			}, BorderLayout.CENTER);
+			pack();
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			setResizable(true);
+		}}.setVisible(true);
 	}
 
 	@Override
@@ -36,5 +50,9 @@ public class Line {
 			return MessageFormat.format("{0}x={1}\tx={2}", this.a, this.c, 0);
 		else
 			return MessageFormat.format("{0}x" + (this.b < 0 ? "" : "+") + "{1}={2}\tx={3}", this.a, this.b, this.c, 0);
+	}
+
+	public static void main(String[] args) {
+		new Line(1, 2, 3).print();
 	}
 }
